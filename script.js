c4ec4d4a54015b1c8a82fba63d6e397e9dc341a3
Shushan Mirzoyan <shushanmirzoyan1@gmail.com>
2023-02-25 00:48:55 +0400

    adding js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const date = new Date();
+document.getElementById("firstspan").innerHTML = date.toLocaleDateString();
+document.getElementById("secondspan").innerHTML = date.toLocaleDateString();
+document.getElementById("thirdspan").innerHTML = date.toLocaleDateString();
+document.getElementById("fourthspan").innerHTML = date.toLocaleDateString()
